68d56e3ac8b36ff6bcaf8b66fd43488c44b7df40
anjali <anjalimalviya2532@gmail.com>
2022-05-09 14:37:04 +0530

    [Anjali] Add: Employee Present Or Absent

---
+++ EmployeeWageProgram/Program1/EmployeeWage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class EmployeeWage
+    {
+        public int EmpPresent = 1;
+        public void CheackEmpPresentAbsent()
+        {
+            Random Check = new Random();
+            int CheckEmp = Check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+                Console.WriteLine("Employeee is present");
+            else
+                Console.WriteLine("EmpPresent is Absent");
+
+        }
+
+    }
+}
---
+++ EmployeeWageProgram/Program1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeWageProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.CheackEmpPresentAbsent();
+        }
+    }
+}
