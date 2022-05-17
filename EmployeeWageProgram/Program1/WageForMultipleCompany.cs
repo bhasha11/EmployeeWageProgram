a9e54eff4785dcd1c9b9488b068d746d7c0224fe
anjali <anjalimalviya2532@gmail.com>
2022-05-17 16:18:59 +0530

    [Anjali]Add: Ability to Check Wage For Multiple Company

---
+++ EmployeeWageProgram/Program1/WageForMultipleCompany.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    internal class WageForMultipleCompany
+    {
+        const int Emp_Full_Time = 1;
+        const int Emp_Part_Time = 2;
+            
+        public int Working_Days_Per_Month = 20;
+        public int Working_Days = 0;
+        public int empHrs = 0;
+        public int EmployeeWage = 0;
+        int totalWage = 0;
+        int totalHours = 0;
+        public void Attendance(string CompanyName, int WagePerHour, int NumWorkingDays, int MaxHoursPerMonth) //
+        {
+            // Stroing RandomValue
+            for (int Day = 1; Day <= Working_Days_Per_Month; Day++)
+            {
+                Random random = new Random(); 
+
+                int empCheck = random.Next(0, 3); 
+                switch (empCheck) 
+                {
+                    case Emp_Full_Time: 
+                        empHrs = WagePerHour * 8;
+                        break;
+
+                    case Emp_Part_Time: 
+                        empHrs = WagePerHour * 4;
+                        break;
+
+                    default:
+                        empHrs = 0; 
+                        break;
+                }
+
+                
+                
+                
+                totalHours += empHrs / 20; 
+                totalWage += empHrs; 
+                if (totalHours >= MaxHoursPerMonth)  
+                    break;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Company Name :" + CompanyName);
+            Console.WriteLine("Number Of Hour Employee  Worked  :" + totalHours);
+            Console.WriteLine("Employee Wage Per Hour :" + WagePerHour);
+            Console.WriteLine("Monthly wage :" + totalWage);
+            Console.WriteLine();
+        }
+    }
+}
+    
+
